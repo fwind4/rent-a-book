92966f66fe36c9d0cb557ea846d2de6116943723
Vass Csaba <vass.csaba@innoweb.ro>
2019-02-23 01:33:27 +0200

    initial setup

---
+++ models/user.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    }
+  }, {});
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};
