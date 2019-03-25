e431087596ce3879b0f23b81fa8de09aa70f83a6
Barni <barnibakos@gmail.com>
2019-03-25 09:34:17 +0200

    indexbovites

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Rent a book' });
 });
+
+router.get('/users', function(req, res, next) {
+  models.User.findAll().then(function (users) {
+    res.send(users);
+  });
+});
+
 
 module.exports = router;
