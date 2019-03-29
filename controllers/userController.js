9b974a19e1cb1884e46c99b2705303941d9b19f6
Barni <barnibakos@gmail.com>
2019-03-29 13:24:01 +0200

    alami

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,25 @@ router.get('/list', function (req, res, next) {
     models.User.findAll().then((users) => {
         res.send(users);
     }).catch((err) => {
-        res.render('error', {message: 'Error', error: err});        
+        res.render('error', { message: 'Error', error: err });
     });
+});
+
+router.get('/signup', function (req, res, next) {
+    res.render('signup');
+});
+
+router.post('/signup', function (req, res, next) {
+    console.log(req.body);    
+    res.send(req.body);
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('signup');
+});
+
+router.post('/login', function (req, res, next) {
+    
 });
 
 module.exports = router;
