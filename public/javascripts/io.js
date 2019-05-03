8695412b66fa72a4f099149b09ea80ae1787dc68
Vass Csaba <vass.csaba@innoweb.ro>
2019-05-03 07:21:43 +0300

    Added socket io and p2p

--- public/javascripts/io.js
+++ public/javascripts/io.js
@@ -4,16 +4,17 @@ if ($('#logged-in').val() === 'true') {
     var p2p = new P2P(socket, opts);
 
     p2p.on("file", function (stream) {
-        console.log(stream);
-
+        window.open((window.URL || window.webkitURL).createObjectURL(new Blob(stream)));
     });
 
     p2p.on("req-file", function (path) {
-        $("#file").change(function (e) {
+        
+        $('#file-path').html(path);
+        $('#transfer-modal').modal('show');
+        $('#transfer-btn').on('click', function (e) {
             ss.forceBase64 = true;
-            var file = e.target.files[0]
-            ss.forceBase64 = true;
-            var file = window.open(path);
+            var input = document.getElementById('file-transfer');
+            var file = input.files[0];
             var stream = ss.createStream();
 
             ss(socket).emit("file", stream, { size: file.size, name: file.name });
@@ -21,7 +22,7 @@ if ($('#logged-in').val() === 'true') {
             var size = 0;
             blobStream.on("data", function (chunk) {
                 size += chunk.length;
-                console.log(Math.floor(size / file.size * 100) + "%");
+                $('#file-path').html(Math.floor(size / file.size * 100) + "%");
             });
             blobStream.pipe(stream);
         });
